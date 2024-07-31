b4a01e944f9b969223639a6ddc409a759914da4e
reisvictor1 <reisvictor448@gmail.com>
2024-07-31 16:29:11 -0300

    finish questions

---
+++ quiz/p4a.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 reisvictor1 <reisvictor448@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  reisvictor1 <reisvictor448@gmail.com>
+ */
+
+#include <stdio.h>
+
+void foo()
+{
+  printf ("Foo\n");
+}
