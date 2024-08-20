9aa4f23ceac97a0518b0cf7fae7cb64aa3dc3947
reisvictor1 <reisvictor448@gmail.com>
2024-08-20 07:00:17 -0300

    add new answers

--- imf/my_lib.c
+++ imf/my_lib.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function made to override string.h lib
+// Function made to override strcmp from string.h lib
 int strcmp(const char *str1, const char *str2) {
     return 0;
 }
